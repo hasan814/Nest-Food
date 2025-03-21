ffd613f124a00862536e52ee8d6ee8bb12406fea
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-21 22:37:51 +0330

    Create Upload and Delete for S3

--- src/modules/s3/s3.service.ts
+++ src/modules/s3/s3.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { extname } from "path";
 import { S3 } from "aws-sdk";
 
 
@@ -16,5 +17,24 @@ export class S3Service {
       region: "default"
     })
   }
-  async uploadFile(file: any, folderName: string) { }
+
+  async uploadFile(file: Express.Multer.File, folderName: string) {
+    const ext = extname(file.originalname)
+    return await this.s3
+      .upload({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `${folderName}/${Date.now()}${ext}`,
+        Body: file.buffer
+      })
+      .promise()
+  }
+
+  async deleteFile(Key: string) {
+    return await this.s3
+      .deleteObject({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: decodeURI(Key)
+      })
+      .promise()
+  }
 }
