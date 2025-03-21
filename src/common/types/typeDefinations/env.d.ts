6bfce416001a08a17e364576e495cb960691652d
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-21 22:25:45 +0330

    Config S3

--- src/common/types/typeDefinations/env.d.ts
+++ src/common/types/typeDefinations/env.d.ts
@@ -8,5 +8,10 @@ namespace NodeJS {
     DB_USERNAME: string;
     DB_PASSWORD: string;
     DB_HOST: string
+    // S3
+    S3_BUCKET_NAME: string;
+    S3_ACCESS_KEY: string;
+    S3_SECRET_KEY: string;
+    S3_ENDPOIT: string;
   }
 }
