bc15ecfc402960e6b4102de651e5b7e462670b00
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-21 22:55:32 +0330

    Added File Intercepor and Decorator

---
+++ src/common/interceptors/upload-file.interceptor.ts
@@ -0,0 +1,9 @@
+import { FileInterceptor } from "@nestjs/platform-express"
+import { memoryStorage } from "multer"
+
+export const UploadFileS3 = (fileName: string) => {
+  return class UploadUtility extends FileInterceptor(fileName, {
+
+    storage: memoryStorage(),
+  }) { };
+}
--- src/modules/category/controllers/category.controller.ts
+++ src/modules/category/controllers/category.controller.ts
@@ -1,8 +1,40 @@
+import { CreateCategoryDto } from '../dto/category.dto';
 import { CategoryService } from '../services/category.service';
-import { Controller } from '@nestjs/common';
+import { SwaggerConsumes } from 'src/common/enums/swagger.enum';
+import { UploadFileS3 } from 'src/common/interceptors/upload-file.interceptor';
+import { ApiConsumes } from '@nestjs/swagger';
+
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) { }
 
+  @Post()
+  @ApiConsumes(SwaggerConsumes.MultipartData)
+  @UseInterceptors(UploadFileS3('image'))
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: /(jpg|jpeg|png|webp)$/ }),
+        ],
+      }),
+    )
+    image: Express.Multer.File,
+
+    @Body() createCategoryDto: CreateCategoryDto,
+  ) {
+    return { image, createCategoryDto };
+  }
 }
